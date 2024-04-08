2447edd4b68d20c3f2888ee6f83c5ac8cf9c3e3f
archiakzam <137755063+archiakzam@users.noreply.github.com>
2024-04-08 23:56:52 +0500

    Update class-5.cpp

--- Новая папка/Klass-lab/work done/class-5.cpp
+++ Новая папка/Klass-lab/work done/class-5.cpp
@@ -4,17 +4,25 @@
 #include "class-5.h"
 using namespace std;
 int main(){
-    student chel("Vanya",18,"math",4);
-    cout<<chel;
-    chel.SetGrade(2);
-    chel.check_grade();
-    cin>>chel;
-    cout<<chel;
-    
-    person Person1("Alice", 25);
-    student Student1("Bob", 20, "Math", 4);
+    system("chcp 1251>null");
 
-   
-    Student1.check_grade();
-    return 0;
+	Vector vec(4);
+	person per;
+	student stud;
+
+	cin >> per;
+	cout << per;
+
+	cin >> stud;
+	cout << stud;
+
+	Object* p = &per;
+	vec.Add(p);
+
+	p = &stud;
+	vec.Add(p);
+
+	cout << vec;
+
+	return 0;
 }
